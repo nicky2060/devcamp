182e0d8e1706a26e7954e7b7641a4c113ea60dbc
Nick Pimentel <nicky2060@gmail.com>
2021-07-01 11:51:56 -0400

    Added Create New and Edit abilities in Portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -7,6 +7,10 @@ class PortfoliosController < ApplicationController
 		@portfolio = Portfolio.new
 	end 
 
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
 	def create
     	@portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
@@ -18,4 +22,16 @@ class PortfoliosController < ApplicationController
       		end
     	end
   	end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end 
 end
