342053d4b77c3e9f070f6a8285f34058b965e640
Nick Pimentel <nicky2060@gmail.com>
2021-07-02 01:14:32 -0400

    Added destroy functionality to Portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -15,6 +15,15 @@ class PortfoliosController < ApplicationController
     @portfolio_item = Portfolio.find(params[:id])
   end 
 
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+
+    @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_path, notice: "Record was successfully destroyed." }
+    end
+  end
+
 	def create
     	@portfolio = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
