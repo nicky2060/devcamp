d5e4d81e8f9483efe5b51c3c6e698553bab844ba
Nick Pimentel <nicky2060@gmail.com>
2021-07-02 11:56:49 -0400

    Added toggle status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_02_134058) do
+ActiveRecord::Schema.define(version: 2021_07_02_151522) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_07_02_134058) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
+    t.integer "status", default: 0
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
   end
 
