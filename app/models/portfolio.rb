6cfadf10fa38887f0f4b0e220626c37563e93ee8
Nick Pimentel <nicky2060@gmail.com>
2021-07-03 01:15:54 -0400

    Added default values for portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
-	validates_presence_of :title, :subtitle, :body
+	validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/600x400"
+		self.thumb_image ||= "https://via.placeholder.com/150x150"
+	end
 end
