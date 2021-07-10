45714782c2d89ee0b1c79ed979cbf650181befc5
Nick Pimentel <nicky2060@gmail.com>
2021-07-09 23:22:26 -0400

    Added nested technology attributes for Portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,13 +1,15 @@
 class Portfolio < ApplicationRecord
-	include Placeholder
-	validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
-
 	has_many :technologies
+	accepts_nested_attributes_for :technologies, 
+								  reject_if: lambda { |attrs| attrs['name'].blank? }
+
+	include Placeholder
+	validates_presence_of :title, :body, :main_image, :thumb_image
 
 	after_initialize :set_defaults
 
 	def set_defaults
-		self.main_image ||= Placeholder.image_generator(height: "600", width: "400")
-		self.thumb_image ||= Placeholder.image_generator(height: "150", width: "150")
+		self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
+		self.thumb_image ||= Placeholder.image_generator(height: '150', width: '150')
 	end
 end
